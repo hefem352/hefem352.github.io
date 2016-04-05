c6f96178029606a15c23956d12cf8c6670b59478
mikey hefez <mikeyhefez@mikey-hefezs-MacBook.local>
2016-04-05 00:51:20 -0400

    message

--- project-6/assets/js/app.js
+++ project-6/assets/js/app.js
@@ -1,21 +1,17 @@
 $('.nav-item').click(function() {
+	$('.nav-item').removeClass('active');
+	$(this).addClass('active');
 
- $('nav-item').removeClass('active');
- $(this).addClass('active');
-	
 	var button_name = $(this).data('button');
-	
+	$('.content').attr('class', 'column content');
+	$('.content').addClass(button_name);
 
-	$('.content').attr('class', 'content column');
-	$('.content').addClass(button_name); 
-
-if (button_name == 'beyonce') {
-	$('.slay').removeClass('hide');
+	if (button_name == 'beyonce') {
+		$('.slay').removeClass('hide');
 	}
 });
 
-$('.close').click(function() {
- $('.slay').addClass('hide');
- $('nav-item').removeClass('active');
+$('.close').click(function(){
+	$('.slay').addClass('hide');
+	$('.nav-item').removeClass('active');
 });
-
