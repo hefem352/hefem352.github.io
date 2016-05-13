54d9ff165071939414d08a350ac2c3878ab1c5a8
mikey hefez <mikeyhefez@mikey-hefezs-MacBook.local>
2016-05-12 22:32:59 -0400

    message

--- psychic/assets/js/app.js
+++ psychic/assets/js/app.js
@@ -13,7 +13,7 @@ var love1 = [" You will", " You will never "," You will alaways "," You will may
     money3 = [" the sun is cold. " , " rats will leave New York. " ];
     passion1 = [" Your inner passion is connected to "]; 
     passion2 = [" bagels and ", " grass and ", " paper bags and " , " chalk and ", " summer breeze and ", " space and ", " design and ", " facbook and ", " sex change and ", " breast milk and ", " children and ", " botox and "];
-    passion3 = [" older pigs. ", "  descreet bushes. ", " paper planes. ", " naked guys. " , " sleeping. ", " magic hummus. "]
+    passion3 = [" older pigs. ", "  discreet bushes. ", " paper planes. ", " naked guys. " , " sleeping. ", " magic hummus. "]
     family1 = ["It seems like something "];
     family2 = [" good ", " bad ", "great ", "strange ", "bizzar ", "scary ", " painful ", "amazing ", " stupid ", "crazy"];
     family3 = [" will happen to "];
@@ -21,6 +21,8 @@ var love1 = [" You will", " You will never "," You will alaways "," You will may
     family5 = [" only if you sleep awake. ", " only if you fly in water. ", "only if you clouds cry tonight. "];
     friends1 =[" Your friends will hate you ", " your friends will be there for you ", " your best friend will steal your lover "];
     friends2 =[" if germans become vegan. ", " if pink is the new black.", " if flip phones become a trend again. ", " if people stop drinking ice coffee. ", " if humans become apes. "];
+
+var end=0
 
 
 $(".love").click(function(){
@@ -32,6 +34,11 @@ $(".textcontainer").append(love2[Math.floor(Math.random() * love2.length)]);
 $(".textcontainer").append(love3[Math.floor(Math.random() * love3.length)]);
 $(".textcontainer").append(love4[Math.floor(Math.random() * love4.length)]);
 
+  end++;
+  
+  console.log(end);
+
+  
 
 });
 
@@ -45,6 +52,12 @@ $(".textcontainer").append(health3[Math.floor(Math.random() * health3.length)]);
 $(".textcontainer").append(health4[Math.floor(Math.random() * health4.length)]);
 $(".textcontainer").append(health5[Math.floor(Math.random() * health5.length)]);
 
+  end++;
+  console.log(end);
+
+  
+
+
 });
 
 $(".money").click(function(){
@@ -54,11 +67,17 @@ $(".money").click(function(){
   $(".textcontainer").append(money1[Math.floor(Math.random() * money1.length)]);
   $(".textcontainer").append(money2[Math.floor(Math.random() * money2.length)]);
   $(".textcontainer").append(money3[Math.floor(Math.random() * money3.length)]);
+
+  end++;
+  
+  console.log(end);
+
+  
+
 });
 
-$(".close").click(function() {
-  $('.textwrapper').hide();
-})
+
+
 $(".passion").click(function(){
    $(this).hide();
    $('.textcontainer').html('');
@@ -66,6 +85,12 @@ $(".passion").click(function(){
   $(".textcontainer").append(passion1[Math.floor(Math.random() * passion1.length)]);
   $(".textcontainer").append(passion2[Math.floor(Math.random() * passion2.length)]);
   $(".textcontainer").append(passion3[Math.floor(Math.random() * passion3.length)]);
+
+  end++;
+  
+  console.log(end);
+
+  
 });
 
 $(".family").click(function(){
@@ -77,6 +102,12 @@ $(".family").click(function(){
   $(".textcontainer").append(family3[Math.floor(Math.random() * family3.length)]);
   $(".textcontainer").append(family4[Math.floor(Math.random() * family4.length)]);
   $(".textcontainer").append(family5[Math.floor(Math.random() * family5.length)]);
+
+  end++;
+  
+  console.log(end);
+
+  
 });
 
 $(".friends").click(function(){
@@ -85,8 +116,30 @@ $(".friends").click(function(){
   $(".textwrapper").show();
   $(".textcontainer").append(friends1[Math.floor(Math.random() * friends1.length)]);
   $(".textcontainer").append(friends2[Math.floor(Math.random() * friends2.length)]);
-  $(".textcontainer").append(friends3[Math.floor(Math.random() * friends3.length)]);
+   
+  end++;
+
+});
+
+$(".close").click(function() {
+  
+  $('.textwrapper').hide();
+
+  if (end === 6){
+    console.log("it worked")
+
+    $('.end').show();
+  };
+
 });
 
 $('.textwrapper').hide();
+
+$('.end').show();
+
+
+
+
+
+
 
